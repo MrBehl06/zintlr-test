7fb8f13ed07cbb590540ab3dbdb3e46b3b55e608
Sandeep Behl <sbehl20-csbs@bvucoep.edu.in>
2023-12-13 21:42:09 +0530

    "Coment out SideBar"

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import logo from "../../assets/logo.png";
-import nav from "../../constants/SidebarInfo.js";
-const Sidebar = () => {
-  return (
-    <div>
-      <aside aria-label="Sidebar">
-        <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 ">
-          <div className="flex mt-8 mb-8  ms-3">
-            <img src={logo} alt="logo" className="h-8 w-8" />
-            <span class="self-center text-xl text-[#6263e8] font-semibold whitespace-nowrap">
-              Zintlr
-            </span>
-          </div>
+// import React from "react";
+// import logo from "../../assets/logo.png";
+// import nav from "../../constants/SidebarInfo.js";
+// const Sidebar = () => {
+//   return (
+//     <div>
+//       <aside aria-label="Sidebar">
+//         <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 ">
+//           <div className="flex mt-8 mb-8  ms-3">
+//             <img src={logo} alt="logo" className="h-8 w-8" />
+//             <span class="self-center text-xl text-[#6263e8] font-semibold whitespace-nowrap">
+//               Zintlr
+//             </span>
+//           </div>
 
-          <ul class="space-y-2 font-medium">
-            {nav.map((nav) => {
-              return (
-                <li key={nav.id}>
-                  <a
-                    href="#"
-                    className="flex items-center p-3 text-gray-900 rounded-full hover:bg-[#29C5F6] hover:text-white group"
-                  >
-                    <nav.logo />
-                    <span className="ms-3">{nav.text}</span>
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </aside>
-    </div>
-  );
-};
-export default Sidebar;
+//           <ul class="space-y-2 font-medium">
+//             {nav.map((nav) => {
+//               return (
+//                 <li key={nav.id}>
+//                   <a
+//                     href="#"
+//                     className="flex items-center p-3 text-gray-900 rounded-full hover:bg-[#29C5F6] hover:text-white group"
+//                   >
+//                     <nav.logo />
+//                     <span className="ms-3">{nav.text}</span>
+//                   </a>
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//         </div>
+//       </aside>
+//     </div>
+//   );
+// };
+// export default Sidebar;
